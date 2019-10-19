2411cab6b35d3dd1c75e5746522feb7c51c2a962
Andri Signorell <andri@signorell.net>
2019-10-19 11:23:04 +0200

    Corrected Untable/Desc.list New WrdBookmark functions

--- src/init.c
+++ src/init.c
@@ -54,12 +54,14 @@ extern SEXP between_num_r(SEXP, SEXP, SEXP);
 extern SEXP between_num_rm(SEXP, SEXP, SEXP);
 extern SEXP conc(SEXP, SEXP, SEXP, SEXP);
 extern SEXP pip(SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
+extern SEXP isnil(SEXP);
 extern SEXP rkurt(SEXP, SEXP);
 extern SEXP rskew(SEXP, SEXP);
 extern SEXP tbrm(SEXP, SEXP);
 extern SEXP _DescTools_dgompertz_work(SEXP, SEXP, SEXP, SEXP);
 extern SEXP _DescTools_pgompertz_work(SEXP, SEXP, SEXP, SEXP, SEXP);
 extern SEXP _DescTools_check_gompertz(SEXP, SEXP);
+extern SEXP _DescTools_fastMode(SEXP, SEXP);
 extern SEXP pKolmogorov2x(SEXP, SEXP);
 
 
@@ -96,6 +98,7 @@ static const R_CallMethodDef CallEntries[] = {
   {"_DescTools_pgompertz_work", (DL_FUNC) &_DescTools_pgompertz_work, 5},
   {"_DescTools_dgompertz_work", (DL_FUNC) &_DescTools_dgompertz_work, 4},
   {"_DescTools_check_gompertz", (DL_FUNC) &_DescTools_check_gompertz, 2},
+  {"_DescTools_fastMode",       (DL_FUNC) &_DescTools_fastMode, 2},
   {"between_num_",             (DL_FUNC) &between_num_,             3},
   {"between_num_l",            (DL_FUNC) &between_num_l,            3},
   {"between_num_lm",           (DL_FUNC) &between_num_lm,           3},
@@ -106,6 +109,7 @@ static const R_CallMethodDef CallEntries[] = {
   {"between_num_rm",           (DL_FUNC) &between_num_rm,           3},
   {"conc",                     (DL_FUNC) &conc,                     4},
   {"pip",                      (DL_FUNC) &pip,                      6},
+  {"isnil",                    (DL_FUNC) &isnil,                    1},
   {"rkurt",                    (DL_FUNC) &rkurt,                    2},
   {"rskew",                    (DL_FUNC) &rskew,                    2},
   {"tbrm",                     (DL_FUNC) &tbrm,                     2},
