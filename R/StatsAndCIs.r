7355c171f3a193ac403067c4b4ce775844c38e7d
Andri Signorell <andri@signorell.net>
2020-08-22 14:32:52 +0200

    0.99.37.012

--- R/StatsAndCIs.r
+++ R/StatsAndCIs.r
@@ -547,6 +547,9 @@ Quantile <- function(x, weights = NULL, probs = seq(0, 1, 0.25),
 }
 
 
+
+
+
 IQRw <- function (x, weights = NULL, na.rm = FALSE, type = 7) {
   
   diff(Quantile(x, weights=weights, probs=c(0.25, 0.75), na.rm=na.rm, type=type))
@@ -3130,6 +3133,103 @@ MedianCI <- function(x, conf.level=0.95, sides = c("two.sided","left","right"), 
   return( res )
 
 }
+
+
+
+
+
+QuantileCI <- function(x, probs=seq(0, 1, .25), conf.level = 0.95, sides = c("two.sided", "left", "right"),
+           na.rm = FALSE, method = c("exact", "boot"), R = 999) {
+  
+  .QuantileCI <- function(x, probs, conf.level = 0.95, sides = c("two.sided", "left", "right")) {
+    # Near-symmetric distribution-free confidence interval for a quantile `q`.
+  
+    # https://stats.stackexchange.com/questions/99829/how-to-obtain-a-confidence-interval-for-a-percentile
+    #
+    # Search over a small range of upper and lower order statistics for the 
+    # closest coverage to 1-alpha (but not less than it, if possible).
+    
+    alpha <- 1- conf.level
+    
+    n <- length(x)
+    
+    u <- qbinom(1-alpha/2, n, probs) + (-2:2) + 1
+    l <- qbinom(alpha/2, n, probs) + (-2:2)
+    u[u > n] <- Inf
+    l[l < 0] <- -Inf
+    coverage <- outer(l, u, function(a, b) pbinom(b-1, n, probs) - pbinom(a-1, n, probs))
+    if (max(coverage) < 1-alpha) i <- which(coverage==max(coverage)) else
+      i <- which(coverage == min(coverage[coverage >= 1-alpha]))
+    # minimal difference
+    i <- i[1]
+    
+    # order statistics and the actual coverage
+    u <- rep(u, each=5)[i]
+    l <- rep(l, 5)[i]
+    
+    # get the values  
+    if(probs %nin% c(0,1))
+      s <- sort(x, partial=c(u, l))
+    else
+      s <- sort(x)
+    
+    res <- c(lwr.ci=s[l], upr.ci=s[u])
+    attr(res, "conf.level") <- coverage[i]
+    
+    if(sides=="left")
+      res[3] <- Inf
+    else if(sides=="right")
+      res[2] <- -Inf
+    
+    return(res)
+    
+  }
+  
+  
+  if (na.rm) x <- na.omit(x)
+  if(anyNA(x))
+    stop("missing values and NaN's not allowed if 'na.rm' is FALSE")
+  
+  sides <- match.arg(sides, choices = c("two.sided","left","right"), several.ok = FALSE)
+  if(sides!="two.sided")
+    conf.level <- 1 - 2*(1-conf.level)
+
+  method <- match.arg(arg=method, choices=c("exact","boot"))
+  
+  switch( method
+          , "exact" = { # this is the SAS-way to do it
+            r <- lapply(probs, function(p) .QuantileCI(x, probs=p, conf.level = conf.level, sides=sides))
+            coverage <- sapply(r, function(z) attr(z, "conf.level"))
+            r <- do.call(rbind, r)
+            attr(r, "conf.level") <- coverage
+          }
+          , "boot" = {
+            boot.med <- boot(x, function(x, d) quantile(x[d], probs=probs, na.rm=na.rm), R=R)
+            r <- boot.ci(boot.med, conf=conf.level, type="basic")[[4]][4:5]
+          } )
+  
+  qq <- quantile(x, probs=probs, na.rm=na.rm)
+  
+  if(length(probs)==1){
+    res <- c(qq, r)
+    names(res) <- c("est","lwr.ci","upr.ci")
+    # report the conf.level which can deviate from the required one
+    if(method=="exact")  attr(res, "conf.level") <-  attr(r, "conf.level")
+    
+  } else {
+    res <- cbind(qq, r)
+    colnames(res) <- c("est","lwr.ci","upr.ci")
+    # report the conf.level which can deviate from the required one
+    if(method=="exact")  
+      # report coverages for all probs
+      attr(res, "conf.level") <-  attr(r, "conf.level")
+    
+  }
+  
+  return( res )
+  
+}
+
 
 
 
