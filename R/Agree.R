32bc6aa787edef05c3dbc750e7f061e16758b1e1
Andri Signorell <andri@signorell.net>
2025-08-14 08:46:48 +0200

    0.99.60.22

--- R/Agree.R
+++ R/Agree.R
@@ -1,39 +1,8 @@
 
 
-# Old version, replace 2025-08-12
-# 
-# Agree <- function(x, tolerance = 0, na.rm = FALSE) {
-# 
-#   x <- as.matrix(x)
-#   if(na.rm) x <- na.omit(x)
-# 
-#   if(anyNA(x)) return(NA)
-# 
-#   ns <- nrow(x)
-#   nr <- ncol(x)
-# 
-#   if (is.numeric(x)) {
-#     rangetab <- apply(x, 1, max) - apply(x, 1, min)
-#     coeff <-  sum(rangetab <= tolerance)/ns
-# 
-#   } else {
-#     rangetab <- as.numeric(sapply(apply(x, 1, table), length))
-#     coeff <- (sum(rangetab == 1)/ns)
-#     tolerance <- 0
-#   }
-# 
-#   rval <- coeff
-#   attr(rval, c("subjects")) <- ns
-#   attr(rval, c("raters")) <- nr
-# 
-#   return(rval)
-# 
-# }
 
 
-
-
-.LongToSquare <- function (formula, data, subset, na.action, ...) {
+.LongToSquare <- function(formula, data, subset, na.action, ...){
   
   # returns a 2-dim matrix of a subject-rater-rating formula
   
@@ -57,13 +26,25 @@
     m$data <- as.data.frame(data)
   
   m[[1L]] <- quote(stats::model.frame)
+  # in order to delete potentially provided tolerance or 
+  # na.rm arguments to be passed later on 
+  m$... <- NULL  
+  
+  
+  ## >>> WICHTIG: subset korrekt behandeln wegen Kollistion mit der Funktion subset
+  if (!missing(subset)) {
+    m$subset <- substitute(subset)  # Ausdruck capturen
+  } else {
+    m$subset <- NULL                # ganz entfernen
+  }
+  ## (Optional) na.action unverändert durchreichen:
+  # if (missing(na.action)) m$na.action <- NULL
+  
+  
   mf <- eval(m, parent.frame())
   
   DNAME <- gettextf("%s by %s (rows) and %s (columns)", 
                     names(mf)[1], names(mf)[2], names(mf)[3])
-  
-  # result of base test
-  # y <- friedman.test(mf[[1L]], mf[[2L]], mf[[3L]])
   
   # now reshaping to matrix form
   m <- reshape(mf, idvar=colnames(mf)[2], timevar=colnames(mf)[3],
@@ -76,14 +57,19 @@
   colnames(m) <- gsub(gettextf("%s\\.", names(mf)[1]), "", colnames(m))
   rownames(m) <- NULL
   
-  if(!is.null(na.action))
+  if(!missing(na.action)){
+    subj <- m[, names(mf)[2]]
     m <- na.action(m)
+    # provide the names of omitted subjects
+    attr(attr(m, "na.action"), "values") <- subj[as.numeric(attr(m, "na.action"))]
+  }
   
   attr(m, "data.name") <- DNAME
-  
+  attr(m, "")
   return(m)
   
 }
+
 
 
 Agree <- function(x,  ...) {
@@ -92,66 +78,16 @@ Agree <- function(x,  ...) {
 
 
 Agree.formula <- function(formula, data, subset, na.action, ...){
-  
-  # returns a 2-dim matrix of a subject-rater-rating formula
-  
-  # originally based on:   stats:::friedman.test.formula
-  
-  if (missing(formula)) 
-    stop("formula missing")
-  
-  if ((length(formula) != 3L) || 
-      (length(formula[[3L]]) != 3L) || 
-      (formula[[3L]][[1L]] != as.name("|")) || 
-      (length(formula[[3L]][[2L]]) != 1L) || 
-      (length(formula[[3L]][[3L]]) != 1L)) 
-    stop("incorrect specification for 'formula'")
-  
-  formula[[3L]][[1L]] <- as.name("+")
-  
-  m <- match.call(expand.dots = FALSE)
-  m$formula <- formula
-  if (is.matrix(eval(m$data, parent.frame()))) 
-    m$data <- as.data.frame(data)
-  
-  m[[1L]] <- quote(stats::model.frame)
-  # in order to delete potential tolerance or na.rm arguments to be passed
-  # later on 
-  m$... <- NULL  
-  mf <- eval(m, parent.frame())
-  
-  DNAME <- gettextf("%s by %s (rows) and %s (columns)", 
-                    names(mf)[1], names(mf)[2], names(mf)[3])
-  
-  # result of base test
-  # y <- friedman.test(mf[[1L]], mf[[2L]], mf[[3L]])
-  
-  # now reshaping to matrix form
-  m <- reshape(mf, idvar=colnames(mf)[2], timevar=colnames(mf)[3],
-               direction="wide")
-  
-  # get better order for rows and columns
-  m <- m[order(m[,1]), ]
-  m <- cbind(m[, 1, drop=FALSE], m[, -1][, order(colnames(m)[-1])])
-  # remove response variable part from columnnames
-  colnames(m) <- gsub(gettextf("%s\\.", names(mf)[1]), "", colnames(m))
-  rownames(m) <- NULL
 
-  if(!missing(na.action)){
-    subj <- m[, names(mf)[2]]
-    m <- na.action(m)
-  }
-
-  attr(m, "data.name") <- DNAME
-
+  m <- .LongToSquare(formula, data, subset, na.action, ...)
   res <- Agree(m[, -1], ...)
-  
+
   if(!is.null(attr(m, "na.action"))){
-    attr(res, "na.action") <- subj[attr(m, "na.action")]
+    attr(res, "na.action") <- attr(m, "na.action")
   }
 
   return(res)
-  
+
 }
 
 
@@ -182,4 +118,34 @@ Agree.default <- function(x, tolerance = 0, na.rm=FALSE, ...){
   
 }
 
+
+# Old version, replaced 2025-08-12
+# 
+# Agree <- function(x, tolerance = 0, na.rm = FALSE) {
+# 
+#   x <- as.matrix(x)
+#   if(na.rm) x <- na.omit(x)
+# 
+#   if(anyNA(x)) return(NA)
+# 
+#   ns <- nrow(x)
+#   nr <- ncol(x)
+# 
+#   if (is.numeric(x)) {
+#     rangetab <- apply(x, 1, max) - apply(x, 1, min)
+#     coeff <-  sum(rangetab <= tolerance)/ns
+# 
+#   } else {
+#     rangetab <- as.numeric(sapply(apply(x, 1, table), length))
+#     coeff <- (sum(rangetab == 1)/ns)
+#     tolerance <- 0
+#   }
+# 
+#   rval <- coeff
+#   attr(rval, c("subjects")) <- ns
+#   attr(rval, c("raters")) <- nr
+# 
+#   return(rval)
+# 
+# }
 
